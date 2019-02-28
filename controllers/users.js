346e8dea8d5855ef7b458bf96fe85f9bcd58e5af
Ajay Kumar <imajkumar@hotmail.com>
2019-02-28 12:14:10 +0530

    joi validation with schema and custom validation is pending

--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,30 @@
-
+const Joi = require('joi');
 function hello (ctx) {
   let user = ctx.request.query.user
   console.log(user);
   ctx.ok({ id: 123, name: 'Dat Boi' });
 }
 function register (ctx) {
-  let email = ctx.request.body.email
-  let password = ctx.request.body.password
-  // console.log(user);
-   ctx.ok({ email :email });
-}
+let email = ctx.request.body.email
+let password = ctx.request.body.password 
+const schema = Joi.object().keys({
+  email: Joi.string().alphanum().min(3).max(30).required(),
+  password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
+  access_token: [Joi.string(), Joi.number()],
+  birthyear: Joi.number().integer().min(1900).max(2013),
+  email: Joi.string().email({ minDomainAtoms: 2 }),
+}).with('email', 'password').without('password', 'access_token');
+
+// Return result.
+const result = Joi.validate({ email: email, password: password }, schema);
+
+ctx.ok({ 
+  'status':0,
+  'message':result.error['message']
+ });
+
+}//end of register
+
 
 
 module.exports = {
